03fd535f2fccfa44b2a376351e14af61c1b1d4cb
michiko lange taguchi <51973693+michi1008@users.noreply.github.com>
2023-08-19 19:55:34 -0700

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,50 +1,46 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
+import ReactDOM from "react-dom";
 import "./index.css";
+import { createRoot } from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+import store from './slices/store';
+import { Provider } from "react-redux";
 import App from "./App";
 import Home from "./pages/Home";
-import ProductsPage from "./pages/ProductsPage";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import ProductsPage from "./pages/ProductsPage";
 import SingleProductPage from "./pages/SingleProductPage";
-import CartPage from "./pages/CartPage";
-import { Provider } from "react-redux";
-import store from "./slices/store";
-import { Auth0Provider } from "@auth0/auth0-react";
+import CartPage from './pages/CartPage';
+
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route index={true} path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/products" element={<ProductsPage />} />
-      <Route path="/products/:id" element={<SingleProductPage />} />
-      <Route path="/cart" element={<CartPage />} />
-    </Route>
-  )
-);
+const root = createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <Auth0Provider
-        domain={domain}
-        clientId={clientId}
-        redirect_uri={window.location.origin}
-      >
-        <RouterProvider router={router} />
-      </Auth0Provider>
-    </React.StrictMode>
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirect_uri={window.location.origin}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index={true} path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/products/:id" element={<SingleProductPage />} />
+            <Route path="/cart" element={<CartPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Auth0Provider>
+    </Provider>
+  </React.StrictMode>
 );
